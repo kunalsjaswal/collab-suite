b99512e0c3a2243ff93f0f47a9f3fd77d0da35cf
Sameer Thakur <samthakur1742002@gmail.com>
2024-02-01 20:44:50 +0530

    socket room

--- back-end/server.js
+++ back-end/server.js
@@ -6,8 +6,23 @@ const path = require('path');
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+// io.on('connection', (socket) => {
+//     console.log('a user connected')
+//     socket.on('chat', (msg) => { console.log(msg); });
+//     socket.emit("hello", "from server")
+//     socket.on('disconnect', () => { console.log('user disconnected'); });
+// });
+let clients = 0
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => { console.log('user disconnected'); });
+    clients++;
+    io.sockets.emit("broadcast", { description: clients + ' clients connected!' });
+    socket.on('disconnect', () => {
+        clients--;
+        io.sockets.emit("broadcast", { description: clients + ' clients connected!' });
+    });
 });
+
+
+
 http.listen(3000, () => { console.log('listening on port 3000'); });
+
